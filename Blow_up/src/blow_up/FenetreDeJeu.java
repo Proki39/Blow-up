65d4e17359aad596321a8947fa9bb2af004353dd
Proki39 <115209807+Proki39@users.noreply.github.com>
2022-10-07 10:04:16 +0200

    fenetre de jeu

---
+++ Blow_up/src/blow_up/FenetreDeJeu.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blow_up;
+
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author lucienboulard
+ */
+public class FenetreDeJeu extends JFrame implements ActionListener, KeyListener {
+    
+    private BufferedImage framebuffer; 
+    private Graphics2D contexte;
+    private JLabel jLabel1;
+    private Jeu jeu;
+    private Timer timer;
+    
+    public FenetreDeJeu() {
+        
+        // initialisation de la fenetre
+        this.setSize(380, 430); 
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        this.jLabel1 = new JLabel();
+        this.jLabel1.setPreferredSize(new java.awt.Dimension(800, 600)); 
+        this.setContentPane(this.jLabel1);
+        this.pack();
+        
+        // Creation du buffer pour l’affichage du jeu et recuperation du contexte graphique
+        this.framebuffer = new BufferedImage(this.jLabel1.getWidth(), this.jLabel1.getHeight(), BufferedImage.TYPE_INT_ARGB); 
+        this.jLabel1.setIcon(new ImageIcon(framebuffer));
+        this.contexte = this.framebuffer.createGraphics();
+        
+        //Creation du jeu
+        this.jeu = new Jeu();
+        
+        // Creation du Timer qui appelle this.actionPerformed() tous les 40 ms
+        this.timer = new Timer(40, this);
+        this.timer.start();
+        }
+        // Methode appelee par le timer et qui effectue la boucle de jeu
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            this.jeu.miseAJour(); this.jeu.rendu(contexte);
+            this.jLabel1.repaint();
+            if (this.jeu.estTermine()) {
+                this.timer.stop(); 
+            }
+        }
+    
+    
+    public static void main(String[] args) {
+        FenetreDeJeu fenetre = new FenetreDeJeu(); 
+        fenetre.setVisible(true);
+    }
+}
---
+++ Blow_up/src/blow_up/Jeu.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blow_up;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author lucienboulard
+ */
+public class Jeu {
+    
+    public void rendu(Graphics2D contexte) { 
+        
+        // 1. Rendu du décor
+        // 2. Rendu des sprites
+        // 3. Rendu des textes
+    }
+    
+    public void miseAJour () {
+        
+        // 1. Mise à jour de l’avatar en fonction des commandes des joueurs
+        // 2. Mise à jour des autres éléments (objets, monstres, etc.)
+        // 3. Gérer les interactions (collisions et autres règles)
+    }
+    
+    public boolean estTermine() {
+        
+        // Renvoie vrai si la partie est terminée (gagnée ou perdue)
+        return false; 
+    }
+    
+}

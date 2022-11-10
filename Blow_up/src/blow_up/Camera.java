6434586a9cc9da0386a1042719824669f2b95ad1
Proki39 <115209807+Proki39@users.noreply.github.com>
2022-11-10 12:23:51 +0100

    caméra

---
+++ Blow_up/src/blow_up/Camera.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blow_up;
+
+/**
+ *
+ * @author lucienboulard
+ */
+public class Camera {
+    public static int camera_x;
+    public static int camera_y;
+}
